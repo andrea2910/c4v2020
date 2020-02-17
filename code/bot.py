c20b2ff719f0df7643d4cef8b20a730f9c647b2d
andrea2910 <amb2335@columbia.edu>
2020-02-16 17:01:30 -0800

    Fix Strings

--- code/bot.py
+++ code/bot.py
@@ -7,38 +7,38 @@ import json
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def bot():
-    os.environ[‘GOOGLE_APPLICATION_CREDENTIALS’] = CONFIG_JSON
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = CONFIG_JSON
     bot = telebot.TeleBot(token = BOT_TOKEN)
-    @bot.message_handler(commands = [‘empieza’])
+    @bot.message_handler(commands = ['empieza'])
     def send_welcome(message):
-        bot.reply_to(message, ‘Bienvenido!’)
-    @bot.message_handler(commands = [‘ayuda’])
+        bot.reply_to(message, 'Bienvenido!')
+    @bot.message_handler(commands = ['ayuda'])
     def send_help(message):
-        bot.reply_to(message, ‘Para usar este canal, escribe el estado en el que se encuentra’)
-    list_states = [‘Amazonas’,
-    ‘Anzoátegui’,
-    ‘Apure’,
-    ‘Aragua’,
-    ‘Barinas’,
-    ‘Bolívar’,
-    ‘Carabobo’,
-    ‘Cojedes’,
-    ‘Delta Amacuro’,
-    ‘Dtto Capital’,
-    ‘Falcón’,
-    ‘Guárico’,
-    ‘Lara’,
-    ‘Mérida’,
-    ‘Miranda’,
-    ‘Monagas’,
-    ‘Nueva Esparta’,
-    ‘Portuguesa’,
-    ‘Sucre’,
-    ‘Táchira’,
-    ‘Trujillo’,
-    ‘Vargas’,
-    ‘Yaracuy’,
-    ‘Zulia’]
+        bot.reply_to(message, 'Para usar este canal, escribe el estado en el que se encuentra')
+    list_states = ['Amazonas',
+    'Anzoátegui',
+    'Apure',
+    'Aragua',
+    'Barinas',
+    'Bolívar',
+    'Carabobo',
+    'Cojedes',
+    'Delta Amacuro',
+    'Dtto Capital',
+    'Falcón',
+    'Guárico',
+    'Lara',
+    'Mérida',
+    'Miranda',
+    'Monagas',
+    'Nueva Esparta',
+    'Portuguesa',
+    'Sucre',
+    'Táchira',
+    'Trujillo',
+    'Vargas',
+    'Yaracuy',
+    'Zulia']
     @bot.message_handler(func = lambda msg: msg.text is not None and msg.text.title() in list_states)
     def location(message):
         #print(message)
@@ -50,18 +50,18 @@ def bot():
         SELECT MAX(insert_date) FROM `hulthack.dashboard_v1`);
         SELECT *
         FROM `hulthack.dashboard_v1`
-        WHERE insert_date = max_date and federal_entity = ‘{}’
+        WHERE insert_date = max_date and federal_entity = '{}'
         “”".format(location)
         df = client.query(sql).to_dataframe()
-        df = df.replace(to_replace=‘-1.0’, value=‘Mal’, regex=True)
-        df = df.replace(to_replace=‘1.0’, value=‘Bien’, regex=True)
-        df = df.replace(to_replace=‘0.0’, value=‘Intermedio’, regex=True)
-        df = df.replace(to_replace=‘-1’, value=‘Mal’, regex=True)
-        df = df.replace(to_replace=‘1’, value=‘Bien’, regex=True)
-        df = df.replace(to_replace=‘0’, value=‘Intermedio’, regex=True)
+        df = df.replace(to_replace='-1.0', value='Mal', regex=True)
+        df = df.replace(to_replace='1.0', value='Bien', regex=True)
+        df = df.replace(to_replace='0.0', value='Intermedio', regex=True)
+        df = df.replace(to_replace='-1', value='Mal', regex=True)
+        df = df.replace(to_replace='1', value='Bien', regex=True)
+        df = df.replace(to_replace='0', value='Intermedio', regex=True)
         output = list()
         for row in df.values:
-            tmp = “”"{1}
+            tmp = """{1}
             Electricidad:\t{3}
             Agua:\t{4}
             Seguridad:\t{5}
@@ -70,9 +70,9 @@ def bot():
             Maternidad:\t{10}
             Asma:\t{13}
             Insulina:\t{14}
-            “”".format(*row)
+            """.format(*row)
             output.append(tmp)
-        bot.reply_to(message, ‘\n\n’.join(output))
+        bot.reply_to(message, '\n\n'.join(output))
     bot.polling()
 
 if __name__ == '__main__':
